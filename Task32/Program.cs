aed08c10ff846b1fe81af66782a4c764e43cca13
Alabuzhev Ruslan <r.alabuzhev@gmail.com>
2023-03-12 19:40:42 +0600

    закончили семинар 3 по С#

---
+++ Task32/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
+
+FillArray(array);
+PrintArray(array);
+EditArray(array);
+PrintArray(array);
+
+void FillArray(int[] arrayValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arrayValue.Length; i++)
+    {
+        arrayValue[i] = rnd.Next(-10,10);
+    }
+}
+
+void PrintArray(int[] arrayValue)
+{
+    for (int i = 0;i < arrayValue.Length;i++)
+    {
+        if (i != arrayValue.Length - 1) Console.Write($"{arrayValue[i]}, ");
+        else Console.Write($"{arrayValue[i]}\n");
+    }
+}
+
+void EditArray(int[] arrayValue)
+{
+    for (int i = 0;i < arrayValue.Length;i++) 
+        array[i] *= -1;    
+}
---
+++ Task37/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Write("Введите размер массива:");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
+FillArray(array);
+PrintArray(array);
+PrintArray(MultiplicationArrayElem(array));
+
+void FillArray(int[] arrayValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arrayValue.Length; i++)
+    {
+        arrayValue[i] = rnd.Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array_value)
+{
+    for (int i = 0; i < array_value.Length; i++)
+    {
+        if (i != array_value.Length - 1) Console.Write($"{array_value[i]}, ");
+        else Console.Write($"{array_value[i]}\n");
+    }
+}
+
+int[] MultiplicationArrayElem(int[] arrayValue)
+{
+    int newArraySize = arrayValue.Length % 2 == 0 ? arrayValue.Length / 2 : arrayValue.Length / 2 + 1;
+    int[] newArray = new int[newArraySize];
+    for (int i = 0; i <= arrayValue.Length; i++)
+    {
+        if (i == newArraySize - 1) 
+        {
+            if (arrayValue.Length % 2 == 0) 
+                newArray[i] = array[i] * array[arrayValue.Length - 1 - i];
+            else 
+                newArray[i] = array[i];
+            break;
+        }
+        newArray[i] = array[i] * array[arrayValue.Length - 1 - i];
+    }
+    return newArray;
+}
