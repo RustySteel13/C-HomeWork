63c2fb7f234a9ac6c28dc075d7094f5af3f16fed
Alabuzhev Ruslan <r.alabuzhev@gmail.com>
2023-03-17 13:36:29 +0600

    Дз семинар №6

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую: ");
+
+string[] strNums = Console.ReadLine().Split(",");
+
+Console.WriteLine(Count(strNums));
+
+int Count(string[] strNums)
+{
+    int cnt = 0; for (int i = 0; i < strNums.Length; i++)
+    {
+        if (Convert.ToInt32(strNums[i].Trim()) > 0)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = GetValue("b1"), k1 = GetValue("k1"), b2 = GetValue("b2"), k2 = GetValue("k2"); double x = GetX(b1, k1, b2, k2), y = GetY(x, b2, k2);
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}; Y: {y}"); double GetX(double b1, double k1, double b2, double k2)
+{
+    return (-b2 + b1) / (-k1 + k2);
+}
+double GetY(double x, double b2, double k2)
+{
+    return k2 * x + b2;
+}
+double GetValue(string name)
+{
+    Console.Write($"Введите {(name.Contains("k") ? "число" : "значение")} {name}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
