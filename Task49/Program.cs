5b7695bdd9270b45e7b3ea189ed305b6bba90af0
Alabuzhev Ruslan <r.alabuzhev@gmail.com>
2023-03-18 22:32:07 +0600

    Закончили семинар №7

---
+++ Task49/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ReplaceNumberToSquare(int[,] matrix)
+{
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <  matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i,j];
+            }
+
+        }
+    }
+}
+
+int[,] array3d = CreateMatrixRndInt(3, 4, 1, 100);
+PrintMatrix(array3d);
+Console.WriteLine();
+ReplaceNumberToSquare(array3d);
+PrintMatrix(array3d);
