761b9726cdce92d4b656bc6d7222f0e5e572cad8
Alabuzhev Ruslan <r.alabuzhev@gmail.com>
2023-03-29 21:16:58 +0600

    Итоговое домашнее задание

---
+++ Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(M, N));
+
+int Sum(int m, int n)
+{
+    return n == m ? m : n + Sum(m, n -1);
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(m, n));
+
+int Akkerman(int m, int n)
+{
+    return n == 0 ? m + 1 : ((n != 0) && (m == 0)) ? Akkerman(n - 1, 1) : Akkerman(n - 1, Akkerman(n - 1, m));
+}
